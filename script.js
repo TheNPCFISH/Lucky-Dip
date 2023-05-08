d6225a7cd45a4431b0b62c6e478e7ea7ae558f6a
NPCFISH <93332534+TheNPCFISH@users.noreply.github.com>
2023-05-08 12:00:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,37 +20,86 @@ function disableName(event) {
 function chooseWinner() {
   const names = document.querySelectorAll(".name");
   const remainingNames = [...names].filter(name => !name.classList.contains("disabled"));
-  const winnerIndex = Math.floor(Math.random() * remainingNames.length);
-  const enabledNames = remainingNames.filter(name => !name.classList.contains("disabled"));
-  const disabledNames = [...names].filter(name => name.classList.contains("disabled"));
-  let delay = 1000; // delay in milliseconds
-  disabledNames.forEach(name => {
-    setTimeout(() => {
-      name.classList.add("hide");
-    }, delay);
-    delay += 1000; // increase delay for next name
-  });
-  setTimeout(() => {
-    enabledNames.forEach((name, index) => {
-      if (index !== winnerIndex) {
-        setTimeout(() => {
-          name.classList.add("hide");
-        }, delay);
-        delay += 1000; // increase delay for next name
-      } else {
-        setTimeout(() => {
-          name.classList.add("winner");
-        }, delay);
-      }
+function Addname() {
+  const newName = document.getElementById("text").value;
+  const nameArea = document.getElementById("namearea");
+  const newDivString =
+    '<div class="name">' +
+    String(newName) +
+    '<button onclick="deleteName(event)" class="delete">X</button></div>';
+  nameArea.innerHTML += newDivString;
+  document.getElementById("text").value = "";
+  addClickListeners();
+}
+
+function deleteName(event) {
+  const divToRemove = event.target.parentNode;
+  divToRemove.remove();
+}
+
+function addClickListeners() {
+  const nameElements = document.querySelectorAll(".name");
+  nameElements.forEach(nameElement => {
+    nameElement.addEventListener("click", event => {
+      event.target.classList.toggle("disabled");
     });
+  });
+}
+
+function chooseWinner() {
+  const names = document.querySelectorAll(".name");
+  const disabledNames = document.querySelectorAll(".name.disabled");
+  const winnerIndex = Math.floor(Math.random() * names.length);
+  let remainingNames = [...names];
+  let delay = 1000; // delay in milliseconds
+
+  // Hide disabled names first
+  disabledNames.forEach(disabledName => {
+    setTimeout(() => {
+      disabledName.classList.add("hide");
+    }, delay);
+    delay += 1000;
+  });
+
+  // Hide non-disabled names one by one
+  while (remainingNames.length > 1) {
+    const randomIndex = Math.floor(Math.random() * remainingNames.length);
+    const nameToRemove = remainingNames[randomIndex];
+    if (nameToRemove !== names[winnerIndex] && !nameToRemove.classList.contains("disabled")) {
+      setTimeout(() => {
+        nameToRemove.classList.add("hide");
+      }, delay);
+      delay += 1000; // increase delay for next name
+    }
+    remainingNames = remainingNames.filter(name => name !== nameToRemove);
+  }
+
+  // Show winner
+  setTimeout(() => {
+    names[winnerIndex].classList.add("winner");
   }, delay);
+
+  // Hide last name after winner is shown
+  setTimeout(() => {
+    remainingNames[0].classList.add("hide");
+  }, delay + 1000);
+
+  // Reset after winner is shown for 5 seconds
   setTimeout(() => {
     names.forEach(name => {
-      name.classList.remove("winner", "hide", "disabled");
+      name.classList.remove("winner", "hide");
       name.classList.add("name");
     });
-  }, delay + 5000); // reset after winner is shown for 5 seconds
+  }, 5000);
 }
+
+var input = document.getElementById("text");
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("add").click();
+  }
+});
 
 function deleteName(event) {
   const divToRemove = event.target.parentNode;
